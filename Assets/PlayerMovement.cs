4e90b1cf6f6708d17e56928b58f8f35b2c2c3023
Jordan <jordan.tibi@inseec-france.com>
2022-08-23 16:31:59 +0200

    input configuration

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] InputActionReference _moveInput;
+    [SerializeField] InputActionReference _jumpInput;
+
+
+}
