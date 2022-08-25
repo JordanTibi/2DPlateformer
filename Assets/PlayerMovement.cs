14c53a4a8ff0cec78b2f89bc802580c7cdc99874
Jordan <jordan.tibi@inseec-france.com>
2022-08-25 15:19:31 +0200

    Jump

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -3,31 +3,69 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+
 public class PlayerMovement : MonoBehaviour
 {
 
     [SerializeField] InputActionReference _moveInput;
     [SerializeField] InputActionReference _jumpInput;
     [SerializeField] Transform _root;
+    [SerializeField] Rigidbody2D rb;
     [SerializeField] float _speed;
+    [SerializeField] float _movingTreshold;
+    [SerializeField] float _force;
+    [SerializeField] float _jumpForce;
+    [SerializeField] Animator _animator;
+    
 
     Vector2 _playerMovement;
+
+   
 
 #if UNITY_EDITOR
     private void Reset()
     {
-        _root = transform.parent;
-        _speed = 2f;
-        Debug.Log("Coucou l'editeur");
+       _root = transform.parent;
+       _speed = 2f;
+       Debug.Log("Coucou l'editeur");
     }
 #endif
-
-    private void Update()
+    
+    private void FixedUpdate()
     {
         Debug.Log(_playerMovement);
 
+        //Movement
         Vector2 direction = new Vector2(_playerMovement.x, 0);
-        _root.transform.Translate(_playerMovement * Time.deltaTime * _speed);
+        _root.transform.Translate(_playerMovement * Time.fixedDeltaTime * _speed, Space.World);
+
+        //Animator
+        Debug.Log($"Magnitude : {direction.magnitude}");
+        if (direction.magnitude > _movingTreshold)    //Si on est en train de bouger
+        {
+            _animator.SetBool("IsWalking", true);
+        }
+        else                   //Sinon que l'on ne bouge pas donc false
+        {
+            _animator.SetBool("IsWalking", false);
+        }
+
+        // Orientation
+
+        
+        if(direction.x > 0)    //Right
+        {
+            _root.rotation = Quaternion.Euler(0, 0, 0);
+            //_root.localScale = new Vector3(1, 1, 1);
+        }
+        else if(direction.x < 0)                    //Left
+        {
+            _root.rotation = Quaternion.Euler(0, 180, 0);
+        }
+
+        //Jump
+
+        
     }
 
     private void Start()
@@ -35,8 +73,22 @@ public class PlayerMovement : MonoBehaviour
         _moveInput.action.started += StartMove;  
         _moveInput.action.performed += UpdateMove;
         _moveInput.action.canceled += EndMove;
+
+        _jumpInput.action.started += JumpStart;
+        
      }
+
     
+
+    private void JumpStart(InputAction.CallbackContext obj)
+    {
+        Debug.Log("Coucou");
+
+        rb.AddForce(new Vector2(0, _jumpForce));
+    }
+
+  
+
     private void StartMove(InputAction.CallbackContext obj)
     {
         
